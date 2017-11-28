0a03d70d4f46b008945b315d37819a9625cca527
ParmBansal <ParmBansal@10.100.138.128>
2017-11-28 09:28:46 -0500

    Allows user to input which piece they want to move

--- ChessAI/src/chessai/ChessAI.java
+++ ChessAI/src/chessai/ChessAI.java
@@ -5,6 +5,8 @@
  */
 package chessai;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ryan and Parm
@@ -15,7 +17,18 @@ public class ChessAI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 	Board b = new Board();
+        int[] ogPos = new int[2];
+        int[] newPos = new int[2];
+        System.out.print("Choose piece to move and position to move it to(eg. A:6-A:4): ");
+        String[] pos = new String[4];
+        pos = input.nextLine().split(":|-");
+        ogPos[0] = (int)pos[0].charAt(0)-65;
+        ogPos[1] = Integer.parseInt(pos[1]);
+        newPos[0] = (int)pos[2].charAt(0)-65;
+        newPos[1] = Integer.parseInt(pos[3]);
+        b.movePiece(ogPos, newPos);
     }
     
 }
