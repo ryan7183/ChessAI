6b3e275f5bbb0514271a43ce607cb730243fe669
Parm Bansal <P4rm8799@gmail.com>
2017-11-29 14:07:44 -0500

    isValid should work

--- ChessAI/src/chessai/Rook.java
+++ ChessAI/src/chessai/Rook.java
@@ -16,37 +16,39 @@ public class Rook extends Piece {
 
     @Override
     public Boolean isValidMove(int[] newPos, BoardSquare[][] bs) {
-            if(this.x==newPos[1]){
-                int changeInY;
-                if(this.y>newPos[0]){
-                    changeInY = -1;
-                }
-                else{
-                    changeInY = 1;
-                }
-                for (int i=this.y+changeInY;i!=newPos[0];i+=changeInY){
-                    if (bs[i][this.x].hasPiece){
-                        System.out.println("First");
-                        //System.out.println(bs[this.x][]);
-                        return false;
-                    }
-                }
+        if(this.x==newPos[1]){
+            int changeInY;
+            if(this.y>newPos[0]){
+                changeInY = -1;
             }
             else{
-                int changeInX;
-                if(this.x>newPos[1]){
-                    changeInX = -1;
-                }
-                else{
-                    changeInX = 1;
-                }
-                for (int i=this.x+changeInX;i!=newPos[1];i+=changeInX){
-                    if (bs[this.y][i].hasPiece){
-                        System.out.println("Second");
-                        return false;
-                    }
+                changeInY = 1;
+            }
+            for (int i=this.y+changeInY;i!=newPos[0];i+=changeInY){
+                if (bs[i][this.x].hasPiece){
+                    System.out.println("First");
+                    //System.out.println(bs[this.x][]);
+                    return false;
                 }
             }
+        }
+        else{
+            int changeInX;
+            if(this.x>newPos[1]){
+                changeInX = -1;
+            }
+            else{
+                changeInX = 1;
+            }
+            for (int i=this.x+changeInX;i!=newPos[1];i+=changeInX){
+                if (bs[this.y][i].hasPiece){
+                    return false;
+                }
+            }
+        }
+        if(bs[newPos[0]][newPos[1]].hasPiece && bs[newPos[0]][newPos[1]].piece.colour == this.colour){
+            return false;
+        }
         return true;
     }
     
