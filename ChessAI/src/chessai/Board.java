21254dd0989e3c1d0905f13e6f45acbd8d1994c8
RyanS <RyanS@10.100.133.81>
2017-11-30 10:18:11 -0500

    X and y should now be more consistent

--- ChessAI/src/chessai/Board.java
+++ ChessAI/src/chessai/Board.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
  * 
  */
 public class Board {
-    BoardSquare[][] board;
+    BoardSquare[][] board;//[y][x]
     Passer p;
     Player player1;
     Player player2;
@@ -23,36 +23,36 @@ public class Board {
 	//Make board
 	for(int y=0;y<8;y++){
 	    for(int x=0;x<8;x++){
-		board[y][x] = new BoardSquare(x,y);
+		board[x][y] = new BoardSquare(x,y);
 	    }
 	}
 	//Set up pieces
 	//Black first row
 	board[0][0].setPiece(new Rook(false,0,0,"R"));
-	board[0][7].setPiece(new Rook(false,7,0,"R"));
-	board[0][1].setPiece(new Knight(false,1,0,"N"));
-	board[0][6].setPiece(new Knight(false,6,0,"N"));
-	board[0][2].setPiece(new Bishop(false,2,0,"B"));
-	board[0][5].setPiece(new Bishop(false,5,0,"B"));
-	board[0][3].setPiece(new Queen(false,3,0,"Q"));
-	board[0][4].setPiece(new King(false,4,0,"K"));
+	board[7][0].setPiece(new Rook(false,7,0,"R"));
+	board[1][0].setPiece(new Knight(false,1,0,"N"));
+	board[6][0].setPiece(new Knight(false,6,0,"N"));
+	board[2][0].setPiece(new Bishop(false,2,0,"B"));
+	board[5][0].setPiece(new Bishop(false,5,0,"B"));
+	board[3][0].setPiece(new Queen(false,3,0,"Q"));
+	board[4][0].setPiece(new King(false,4,0,"K"));
 	//Black pawns
 	for(int x=0;x<8;x++){
-	    board[1][x].setPiece(new Pawn(false,x,1,"P"));
+	    board[x][1].setPiece(new Pawn(false,x,1,"P"));
 	
 	}
 	//White first row
-	board[7][0].setPiece(new Rook(true,0,7,"r"));
+	board[0][7].setPiece(new Rook(true,0,7,"r"));
 	board[7][7].setPiece(new Rook(true,7,7,"r"));
-	board[7][1].setPiece(new Knight(true,1,7,"n"));
-	board[7][6].setPiece(new Knight(true,6,7,"n"));
-	board[7][2].setPiece(new Bishop(true,2,7,"b"));
-	board[7][5].setPiece(new Bishop(true,05,7,"b"));
-	board[7][3].setPiece(new Queen(true,3,7,"q"));
-	board[7][4].setPiece(new King(true,4,7,"k"));
+	board[1][7].setPiece(new Knight(true,1,7,"n"));
+	board[6][7].setPiece(new Knight(true,6,7,"n"));
+	board[2][7].setPiece(new Bishop(true,2,7,"b"));
+	board[5][7].setPiece(new Bishop(true,05,7,"b"));
+	board[3][7].setPiece(new Queen(true,3,7,"q"));
+	board[4][7].setPiece(new King(true,4,7,"k"));
 	//White pawns
 	for(int x=0;x<8;x++){
-	    board[6][x].setPiece(new Pawn(true,x,6,"p"));
+	    board[x][6].setPiece(new Pawn(true,x,6,"p"));
 	
 	}
 	p.setBoard(board);
@@ -86,7 +86,6 @@ public class Board {
 		while(true){
 		    pieceSelected = player1.requestPiece();//Get piece to be moved
 		    valid = validPiece(player1,pieceSelected);
-
 		    if(valid){
 			p.setSelectionPosition(pieceSelected);
 			p.drawSelection=true;
@@ -101,10 +100,12 @@ public class Board {
 		    if(p.cancelSelection){
 			break;
 		    }
-		    if(board[pieceMove[1]][pieceMove[0]].piece.isValidMove(pieceMove, board)){
+		    if(board[pieceMove[0]][pieceMove[1]].piece.isValidMove(pieceMove, board)){
+			System.out.println(2);
 			requestMove(pieceSelected,pieceMove);//move piece
 			break;
 		    }
+		    System.out.println(3);
 		    System.out.println("Can't move a piece like that");
 		}
 		if(p.cancelSelection){
@@ -144,7 +145,8 @@ public class Board {
 		    if(p.cancelSelection){
 			break;
 		    }
-		    if(board[pieceMove[1]][pieceMove[0]].piece.isValidMove(pieceMove, board)){
+		    
+		    if(board[pieceMove[0]][pieceMove[1]].piece.isValidMove(pieceMove, board)){
 			requestMove(pieceSelected,pieceMove);//Move piece
 			break;
 		    }
@@ -169,19 +171,19 @@ public class Board {
     
     BoardSquare[][] requestMove(int[] piece, int[] move){
 	int x,y;
-	board[move[1]][move[0]].piece = board[piece[1]][piece[0]].piece;
-	board[move[1]][move[0]].piece.x = move[0];
-	board[move[1]][move[0]].piece.y = move[1];
-        board[move[1]][move[0]].piece.hasMoved = true;
-	board[piece[1]][piece[0]].hasPiece=false;
-	board[move[1]][move[0]].hasPiece=true;
+	board[move[0]][move[1]].piece = board[piece[0]][piece[1]].piece;
+	board[move[0]][move[1]].piece.x = move[0];
+	board[move[0]][move[1]].piece.y = move[1];
+        board[move[0]][move[1]].piece.hasMoved = true;
+	board[piece[0]][piece[1]].hasPiece=false;
+	board[move[0]][move[1]].hasPiece=true;
 	return board;
     }
     
     boolean validPiece(Player player, int[] c){
 	boolean valid = true;
 	
-	if(!board[c[1]][c[0]].hasPiece || player.colour!=board[c[1]][c[0]].piece.colour){
+	if(!board[c[0]][c[1]].hasPiece || player.colour!=board[c[0]][c[1]].piece.colour){
 	    valid = false;
 	}
 	return valid;
