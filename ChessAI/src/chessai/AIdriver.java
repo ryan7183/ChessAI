34c3c4ba59ef8b3f73277e7a53efcf8fafe07659
Ryan Seguin <PublicEmailRyan@gmail.com>
2017-12-24 11:58:18 -0500

    Added movelist to generate moves call in aidriver

--- ChessAI/src/chessai/AIdriver.java
+++ ChessAI/src/chessai/AIdriver.java
@@ -5,6 +5,10 @@
  */
 package chessai;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Ryan and Parm
@@ -14,6 +18,7 @@ public class AIdriver extends Player{
     int[] move;
     int[] pieceChosen;
     final int Max_Depth=2; 
+    ArrayList<Piece> randList= new ArrayList();
     AIdriver(Passer p, boolean colour){
 	this.colour = colour;
 	passer=p;
@@ -25,6 +30,7 @@ public class AIdriver extends Player{
     @Override
     int[] requestPiece(BoardSquare[][] bs) {
 	double value=alphaBeta(bs,0,Double.NEGATIVE_INFINITY,Double.POSITIVE_INFINITY);
+	System.out.println(pieceChosen[0]+","+pieceChosen[1]+" to "+move[0]+","+move[1]);
 	return pieceChosen;
     }
     
@@ -80,16 +86,21 @@ public class AIdriver extends Player{
      */
     
     double alphaBeta(BoardSquare[][] bs,int depth,double a, double b){
+	//printBoard(bs);
 	Piece[] p=getPieces(bs,colour);
 	int[][] moves;
 	int[] piecePos=new int[2];
+	int[] tempPos = new int[2];
 	double best=Double.NEGATIVE_INFINITY;
 	double value=Double.NEGATIVE_INFINITY;
 	BoardSquare[][] newBoard;
 	for(int i=0;i<p.length;i++){
-	    moves = p[i].generateMoves(bs);
+	    moves = p[i].generateMoves(bs,randList);
 	    for(int j=0;j<moves.length;j++){
 		newBoard = copyBoard(bs);
+		/*tempPos[0] = p[i].x;
+		tempPos[1] =p[i].y;
+		newBoard = requestMove(tempPos,moves[j],newBoard);*/
 		value =  minValue(newBoard,0,Double.NEGATIVE_INFINITY,Double.POSITIVE_INFINITY);
 		if(value>best){
 		    best =value;
@@ -100,23 +111,29 @@ public class AIdriver extends Player{
 		}
 	    }
 	}
+	
 	return best;
     }
     
     double maxValue(BoardSquare[][] bs,int depth,double a, double b){
+	//printBoard(bs);
 	double v;
 	Piece[] p=getPieces(bs,colour);
 	int[][] moves;
 	BoardSquare[][] newBoard;
 	int[] piecePos=new int[2];
+	int[] tempPos = new int[2];
 	if(depth >= Max_Depth||isCheckMate(!colour,bs)){
 	    return boardEvaluation(bs);
 	}
 	v=Double.NEGATIVE_INFINITY;
 	for(int i=0;i<p.length;i++){
-	    moves = p[i].generateMoves(bs);
+	    moves = p[i].generateMoves(bs,randList);
 	    for(int j=0;j<moves.length;j++){
 		newBoard = copyBoard(bs);
+		/*tempPos[0] = p[i].x;
+		tempPos[1] =p[i].y;
+		newBoard = requestMove(tempPos,moves[j],newBoard);*/
 		v = max(v,minValue(newBoard,depth+1,a,b));
 		if(v<b){
 		    return v;
@@ -128,19 +145,24 @@ public class AIdriver extends Player{
     }
     
     double minValue(BoardSquare[][] bs,int depth,double a, double b){
+	//printBoard(bs);
 	double v;
 	Piece[] p=getPieces(bs,!colour);
 	int[][] moves;
 	BoardSquare[][] newBoard;
 	int[] piecePos=new int[2];
+	int[] tempPos = new int[2];
 	if(depth >= Max_Depth||isCheckMate(colour,bs)){
 	    return boardEvaluation(bs);
 	}
 	v=Double.POSITIVE_INFINITY;
 	for(int i=0;i<p.length;i++){
-	    moves = p[i].generateMoves(bs);
+	    moves = p[i].generateMoves(bs,randList);
 	    for(int j=0;j<moves.length;j++){
 		newBoard = copyBoard(bs);
+		/*tempPos[0] = p[i].x;
+		tempPos[1] =p[i].y;
+		newBoard = requestMove(tempPos,moves[j],newBoard);*/
 		v = min(v,maxValue(newBoard,depth+1,a,b));
 		if(v<a){
 		    return v;
@@ -167,55 +189,21 @@ public class AIdriver extends Player{
     
     double boardEvaluation(BoardSquare[][] bs){
 	double score=0;//Greater value beter for this player
-	score = scoreFromNumPieces(bs)+scoreFromPawnDistanceToEnd(bs);
-	return score;
-    }
-    
-    double scoreFromNumPieces(BoardSquare[][] bs){
-	double score=0;
-	int blackPieceCount=0;
-	int whitePieceCount=0;
-	//Count the number of pieces each player has
-	for(BoardSquare[] bArray:bs){
-	    for(BoardSquare s:bArray){
-		if(s.hasPiece){
-		    if(s.piece.colour){
-			//Search for white
-			whitePieceCount++;
-		    }else{
-			//search for black
-			blackPieceCount++;
-		    }
-		}
-	    }
+	ScoreFromNumPieces one = new ScoreFromNumPieces(bs,colour);
+	ScoreFromPawnDistanceToEnd two = new ScoreFromPawnDistanceToEnd(bs,colour);
+	ScoreFromPieceValue three = new ScoreFromPieceValue(bs,colour);
+	one.start();
+	two.start();
+	three.start();
+	try {
+	    one.join();
+	    two.join();
+	    three.join();
+	    score = one.score+two.score+three.score;
+	} catch (InterruptedException ex) {
+	    
 	}
-	if(colour/*white*/){
-	    score = score + whitePieceCount+(16-blackPieceCount);//White pieces currently in play plus pieces black lost
-	}else{
-	    score = score+ blackPieceCount+(16-whitePieceCount);//Black pieces currently in play plus pieces white lost
-	}
-	return score;
-    }
-    
-    double scoreFromPawnDistanceToEnd(BoardSquare[][] bs){
-	double score=0;
-	if(colour/*white*/){
-	    for(int x=0;x<bs.length;x++){
-		for(int y=0;y<bs[0].length;y++){
-		    if(bs[x][y].hasPiece&&bs[x][y].piece.colour&&bs[x][y].piece.textRepresentation.equals("p")){
-			score = score+(8-bs[x][y].piece.y);
-		    }
-		}
-	    }
-	}else{
-	    for(int x=0;x<bs.length;x++){
-		for(int y=0;y<bs[0].length;y++){
-		    if(bs[x][y].hasPiece&&!bs[x][y].piece.colour&&bs[x][y].piece.textRepresentation.equals("P")){
-			score = score+(bs[x][y].piece.y);
-		    }
-		}
-	    }
-	}
+	//score = scoreFromNumPieces(bs)+scoreFromPawnDistanceToEnd(bs);
 	return score;
     }
     
@@ -250,7 +238,7 @@ public class AIdriver extends Player{
 	for(int x=0;x<bs.length;x++){
 	    for(int y=0;y<bs[0].length;y++){
 		if(bs[x][y].hasPiece && bs[x][y].piece.colour==c){
-		    moves = bs[x][y].piece.generateMoves(board);
+		    moves = bs[x][y].piece.generateMoves(board,randList);
                     if(pieceCanPreventCheck(kingPos,moves,board,c)){
                         checkMate = false;
                     }
@@ -285,11 +273,6 @@ public class AIdriver extends Player{
     boolean pieceCanPreventCheck(int[] pos,int[][] moves, BoardSquare[][] board, boolean c){
 	
 	BoardSquare[][] bs = new BoardSquare[board.length][board[0].length];
-	//Make copy of array to work on
-	/*for(int  i=0;i<bs.length;i++){
-	    bs[i]=Arrays.copyOf(board[i],board[i].length);
-	    //System.arraycopy(board[i], 0, bs[i], 0, bs.length);
-	}*/
 	for(int x=0;x<bs.length;x++){
 	    for(int y=0;y<bs[0].length;y++){
 		bs[x][y] = new BoardSquare(board[x][y]);
@@ -316,7 +299,7 @@ public class AIdriver extends Player{
         for(int x=0; x<board.length; x++){
             for(int y=0; y<board[0].length; y++){
                 if(board[x][y].hasPiece && board[x][y].piece.colour != c){
-                    if(board[x][y].piece.isValidMove(loc, board)){
+                    if(board[x][y].piece.isValidMove(loc, board,randList)){
                         return true;
                     }
                 }
@@ -324,5 +307,212 @@ public class AIdriver extends Player{
         }
         return false;
     }
+    
+    void printBoard(BoardSquare[][] bs){
+	for(int y=0;y<bs.length;y++){
+	    for(int x=0;x<bs[0].length;x++){
+		if(bs[x][y].hasPiece){
+		    System.out.print("["+bs[x][y].piece.textRepresentation+"]");
+		}else{
+		    System.out.print("[ ]");
+		}
+	    }
+	    System.out.println();
+	}
+	System.out.println();
+    }
+    
+    //Threads for evaluating board
+    class ScoreFromNumPieces extends Thread{
+	double score;
+	BoardSquare[][] bs;
+	Boolean colour;
+	ScoreFromNumPieces(BoardSquare[][] bs,Boolean colour){
+	    this.bs = bs;
+	    this.score =0;
+	    this.colour = colour;
+	}
+	@Override
+	public void run(){
+	   int blackPieceCount=0;
+	   int whitePieceCount=0;
+	    //Count the number of pieces each player has
+	    for(BoardSquare[] bArray:bs){
+		for(BoardSquare s:bArray){
+		    if(s.hasPiece){
+			if(s.piece.colour){
+			    //Search for white
+			    whitePieceCount++;
+			}else{
+			    //search for black
+			    blackPieceCount++;
+			}
+		    }
+		}
+	    }
+	    if(colour/*white*/){
+		score = score + whitePieceCount+(16-blackPieceCount);//White pieces currently in play plus pieces black lost
+	    }else{
+		score = score+ blackPieceCount+(16-whitePieceCount);//Black pieces currently in play plus pieces white lost
+	    }
+	}
+    }
+    
+    class ScoreFromPawnDistanceToEnd extends Thread{
+	double score;
+	BoardSquare[][] bs;
+	Boolean colour;
+	
+	ScoreFromPawnDistanceToEnd(BoardSquare[][] bs,Boolean colour){
+	    this.score = 0;
+	    this.bs = bs;
+	    this.colour = colour;
+	}
+	@Override
+	public void run(){
+	    if(colour/*white*/){
+		for(int x=0;x<bs.length;x++){
+		    for(int y=0;y<bs[0].length;y++){
+			if(bs[x][y].hasPiece&&bs[x][y].piece.colour==colour&&bs[x][y].piece.textRepresentation.equals("p")){
+			    score = score+(8-bs[x][y].piece.y);
+			}
+		    }
+		}
+	    }else{
+		for(int x=0;x<bs.length;x++){
+		    for(int y=0;y<bs[0].length;y++){
+			if(bs[x][y].hasPiece&&!bs[x][y].piece.colour==colour&&bs[x][y].piece.textRepresentation.equals("P")){
+			    score = score+(bs[x][y].piece.y);
+			}
+		    }
+		}
+	    }
+	}
+    }
+    
+    class ScoreFromPieceValue extends Thread{
+	double score;
+	BoardSquare[][] bs;
+	Boolean colour;
+	
+	ScoreFromPieceValue(BoardSquare[][] bs,Boolean colour){
+	    this.score = 0;
+	    this.bs = bs;
+	    this.colour = colour;
+	}
+	@Override
+	public void run(){
+	    if(/*white*/colour){
+		for(int x=0;x<bs.length;x++){
+		    for(int y=0;y<bs[0].length;y++){
+			if(bs[x][y].hasPiece&&bs[x][y].piece.colour==colour){
+			    switch(bs[x][y].piece.textRepresentation){
+				case "r":
+				    //Rook
+				    score = score+5;
+				    break;
+				case "n":
+				    //Knight
+				    score = score +3;
+				    break;
+				case "b":
+				    //Bishop
+				    score = score +3;
+				    break;
+				case "k":
+				    //King
+				    score = score +100;
+				    break;
+				case "p":
+				    score = score +1;
+				    //Pawn
+				    break;
+				default:
+				    break;
+			    }
+			}
+		    }
+		}
+	    }else{
+		for(int x=0;x<bs.length;x++){
+		    for(int y=0;y<bs[0].length;y++){
+			if(bs[x][y].hasPiece&&bs[x][y].piece.colour==colour){
+			    switch(bs[x][y].piece.textRepresentation){
+				case "R":
+				    //Rook
+				    score = score+5;
+				    break;
+				case "N":
+				    //Knight
+				    score = score +3;
+				    break;
+				case "B":
+				    //Bishop
+				    score = score +3;
+				    break;
+				case "K":
+				    //King
+				    score = score +100;
+				    break;
+				case "P":
+				    score = score +1;
+				    //Pawn
+				    break;
+				default:
+				    break;
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
+    /*double scoreFromNumPieces(BoardSquare[][] bs){
+	double score=0;
+	int blackPieceCount=0;
+	int whitePieceCount=0;
+	//Count the number of pieces each player has
+	for(BoardSquare[] bArray:bs){
+	    for(BoardSquare s:bArray){
+		if(s.hasPiece){
+		    if(s.piece.colour){
+			//Search for white
+			whitePieceCount++;
+		    }else{
+			//search for black
+			blackPieceCount++;
+		    }
+		}
+	    }
+	}
+	if(colour/*white){
+	    score = score + whitePieceCount+(16-blackPieceCount);//White pieces currently in play plus pieces black lost
+	}else{
+	    score = score+ blackPieceCount+(16-whitePieceCount);//Black pieces currently in play plus pieces white lost
+	}
+	return score;
+    }
+    
+    double scoreFromPawnDistanceToEnd(BoardSquare[][] bs){
+	double score=0;
+	if(colour/*white){
+	    for(int x=0;x<bs.length;x++){
+		for(int y=0;y<bs[0].length;y++){
+		    if(bs[x][y].hasPiece&&bs[x][y].piece.colour&&bs[x][y].piece.textRepresentation.equals("p")){
+			score = score+(8-bs[x][y].piece.y);
+		    }
+		}
+	    }
+	}else{
+	    for(int x=0;x<bs.length;x++){
+		for(int y=0;y<bs[0].length;y++){
+		    if(bs[x][y].hasPiece&&!bs[x][y].piece.colour&&bs[x][y].piece.textRepresentation.equals("P")){
+			score = score+(bs[x][y].piece.y);
+		    }
+		}
+	    }
+	}
+	return score;
+    }*/
 }
 
