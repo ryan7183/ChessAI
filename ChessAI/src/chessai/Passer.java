7b4f279f6e839414383dc721109dfc7873c8eb4d
RyanS <RyanS@Ryan-Surface>
2017-12-30 14:05:03 -0500

    Passer has been commented

--- ChessAI/src/chessai/Passer.java
+++ ChessAI/src/chessai/Passer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chessai;
 
 import javafx.stage.Stage;
@@ -10,64 +5,67 @@ import javafx.stage.Stage;
 /**
  *
  * @author RyanS
+ * A class used to pass values between threads
  */
 public class Passer {
-    volatile boolean boardUpdate;
-    volatile BoardSquare[][] board;
-    volatile double mouseX;
-    volatile double mouseY;
-    volatile boolean mouseClicked;
-    volatile boolean playerSelect;
-    volatile boolean playerDone;
-    volatile boolean request;
-    volatile boolean mouseUpdate;
-    volatile boolean drawSelection;
-    volatile boolean cancelSelection;
-    volatile boolean isInvalid;
-    volatile boolean whiteCheckmate;
-    volatile boolean blackCheckmate;
-    volatile boolean stalemate;
+    volatile boolean boardUpdate;//True if board visuals need to be updated
+    volatile BoardSquare[][] board;//The current board state
+    volatile double mouseX;//Mouse click x value
+    volatile double mouseY;//Mouse click y value
+    volatile boolean mouseClicked;//True if the mouse has been clicked
+    volatile boolean playerSelect;//True if the player has selected a piece
+    volatile boolean drawSelection;//True if the selection of a piece should be displayed
+    volatile boolean cancelSelection;//True if the undo select button has been pressed
+    volatile boolean whiteCheckmate;//True if white is in check mate
+    volatile boolean blackCheckmate;//True if black is in check mate
+    volatile boolean stalemate;//True if board is in stalemate
     volatile boolean promotion;
-    volatile boolean newBoardAvailable;
-    volatile BoardSquare[][] newBoard;
+    volatile boolean newBoardAvailable;//True if a new board has been loaded
+    volatile boolean isInvalid;
+    volatile BoardSquare[][] newBoard;//The new board
     volatile String newPiece;
-    volatile int[] selectionPosition;
+    volatile int[] selectionPosition;//The posiition of the selection box
     Passer(){
 	cancelSelection = false;
 	playerSelect = false;
-	playerDone = false;
 	boardUpdate =true;
 	board = null;
 	mouseX=0;
 	mouseY = 0;
 	drawSelection=false;
-        isInvalid = false;
         whiteCheckmate = false;
         blackCheckmate = false;
         stalemate = false;
         promotion = false;
+	isInvalid = false;
         newPiece = "";
 	selectionPosition = new int[2];
 	newBoard = null;
 	newBoardAvailable=false;
     }
+    
+    /**Sets the position for the selection box
+     * @param p the position
+     */
     public void setSelectionPosition(int[] p){
 	selectionPosition[0] =54+( p[0]*73);
 	selectionPosition[1] =60+(p[1]*74);
     }
     
+    /**Sets the board state
+     * @param b the new board state
+     */
     public void setBoard(BoardSquare[][] b){
 	board = b;
     }
     
+    /**Sets if the player has selected a piece
+     */
     public void playerSelectSet(Boolean b){
 	playerSelect = b;
     }
     
-    public void playerDoneSet(Boolean b){
-	playerDone = b;
-    }
-    
+    //Sets the new board
     public void setNewBoard(BoardSquare[][] bs){
 	this.newBoard=bs;
 	board=bs;
